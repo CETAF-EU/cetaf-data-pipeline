3a73600a22b876b11a456c529fdea027fd98281e
Aglorios17 <alessio.glorioso17@gmail.com>
2026-01-16 14:27:56 +0100

    new Dockerfile

--- cetaf_survey_api/cetaf_survey_api/settings.py
+++ cetaf_survey_api/cetaf_survey_api/settings.py
@@ -139,31 +139,31 @@ REST_FRAMEWORK = {
     ]
 }
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'verbose': {
-            'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s'
-        }
-    },
-    'handlers': {
-        'gunicorn': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'formatter': 'verbose',
-            'filename': '/opt/cetaf_passport_api/logs/gunicorn.errors',
-            'maxBytes': 1024 * 1024 * 100,  # 100 mb
-        }
-    },
-    'loggers': {
-        'gunicorn.errors': {
-            'level': 'DEBUG',
-            'handlers': ['gunicorn'],
-            'propagate': True,
-        },
-    }
-}
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': True,
+#     'formatters': {
+#         'verbose': {
+#             'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s'
+#         }
+#     },
+#     'handlers': {
+#         'gunicorn': {
+#             'level': 'DEBUG',
+#             'class': 'logging.handlers.RotatingFileHandler',
+#             'formatter': 'verbose',
+#             # 'filename': '/opt/cetaf_passport_api/logs/gunicorn.errors',
+#             'maxBytes': 1024 * 1024 * 100,  # 100 mb
+#         }
+#     },
+#     'loggers': {
+#         'gunicorn.errors': {
+#             'level': 'DEBUG',
+#             'handlers': ['gunicorn'],
+#             'propagate': True,
+#         },
+#     }
+# }
 ####
 CETAF_DATA_ADDRESS={
         "institutions": "",
